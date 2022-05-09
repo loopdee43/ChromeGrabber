becd6b0e1a398690863fff4b4d9fcb30dae9c72a
loopdee43 <90978974+loopdee43@users.noreply.github.com>
2022-05-09 12:46:59 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-from os.path import join
+import os
 PORT = int(input("input any port over 1212: "))
 s = socket.socket()
 s.bind((socket.gethostname(), PORT))
@@ -7,8 +7,7 @@ s.listen(1)
 print("waiting for connections...")
 conn, addr = s.accept()
 print(f"{addr} has connected!")
-path = input("input the path where everything will be saved: ")
 while True:
     data = conn.recv(1024)
-    thingy = open(join(path, data), 'w')
+    thingy = open(data, 'w')
 
